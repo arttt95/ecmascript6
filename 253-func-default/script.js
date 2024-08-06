58626c3bf5ddd9c94c84fe5b98742257acb072f7
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-06 03:39:17 -0300

    05AGO24 - Aula 253 - Funções (Parâmetros default)

---
+++ 253-func-default/script.js
@@ -0,0 +1,5 @@
+function contarUmaHistoria (personagem = 'Fubá', atividade = 'correr no parque', nome_dono = 'João') {
+    document.write(`Era uma vez um cachorro chamado ${personagem}, ele adorava ${atividade}, seu dono era o ${nome_dono} e eles viveram felizes para sempre`)
+}
+
+contarUmaHistoria(undefined, undefined, 'Pedro')
