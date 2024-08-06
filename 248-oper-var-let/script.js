c300789b6ca17cc1cbf12150649c1c98a74c3b64
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-06 01:59:31 -0300

    06AGO24 - Aula 248 - Operadores de declaração de variáveis Var e Let

---
+++ 248-oper-var-let/script.js
@@ -0,0 +1,34 @@
+//03 escopos: global, função e bloco
+
+/*
+function x () {
+
+    
+
+    if (true) {
+
+        var serie = 'The Walking Dead'
+        
+    }
+
+    console.log('dentro da função: ' + serie)
+}
+
+x()
+
+if (true) {
+
+
+
+    console.log(serie)
+
+}
+
+console.log(serie)*/
+
+if (true) {
+    let serie = 'Friends'
+    console.log(serie)
+}
+
+console.log(serie)
